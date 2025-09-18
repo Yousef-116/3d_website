3f68978590a554a33a077a45dd9adb9aa42162a2
Yousef-116 <yousef.com1162003@gmail.com>
2025-09-19 00:58:43 +0300

    add Header, menu for mobile

--- 3d_website/src/App.jsx
+++ 3d_website/src/App.jsx
@@ -1,34 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from "./components/Header"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <main>
+      {/* // Image */}
+      <img className="absolute top-0 right-0
+      opacity-60 -z-10" src="/gradient.png" alt="grad-img" />
+      {/* // Blur Effect */}
+      <div className="h-0 w-[40rem] absolute top-[20%]
+      right-[-5%] shadow-[0_0_900px_20px_#e99b63] -rotate-[30deg] -z-10"></div>
+      <Header />
+    
+   </main>
   )
 }
 
---
+++ 3d_website/src/components/Header.jsx
@@ -0,0 +1,84 @@
+import React from 'react'
+import 'boxicons/css/boxicons.min.css';
+
+const Header = () => {
+    //toggle menu
+    const toggleMenu = () => {
+        const mobileMenu = document.getElementById('mobileMenu');
+
+        if (mobileMenu.classList.contains('hidden')) {
+            mobileMenu.classList.remove('hidden');
+        } else {
+            mobileMenu.classList.add('hidden');
+        }
+    }
+
+
+    return (
+        <header className="flex justify-between items-center 
+            py-4 px-4 lg:px-20">
+            <h1 className="text-3xl md:text-4xl lg:text-5xl
+         ">MCODE</h1>
+
+            <nav className='hidden md:flex items-center
+             gap-8 lg:gap-12'>
+                <a className='text-base tracking-wider
+                transition-colors hover:text-gray-300
+                z-50' href="#">COMPANY</a>
+
+                <a className='text-base tracking-wider
+                transition-colors hover:text-gray-300
+                z-50' href="#">FEATURES</a>
+
+                <a className='text-base tracking-wider
+                transition-colors hover:text-gray-300
+                z-50' href="#">RESOURCES</a>
+
+                <a className='text-base tracking-wider
+                transition-colors hover:text-gray-300
+                z-50' href="#">DOCS</a>
+
+            </nav>
+
+            <button className='hidden md:block
+            bg-[#a7a7a7] text-black 
+            py-3 px-8 rounded-full border-none
+            font-medium transition-all duration-500 
+            hover:bg-white cursor-pointer z-50'>SIGNIN</button>
+
+            {/* menu for Mobile */}
+            <button onClick={toggleMenu} className='md:hidden
+            text-white  
+            text-3xl z-50
+            px-1 rounded-full border-none    '> <i class='bx bx-menu'></i></button>
+
+            {/* sidebar */}
+            <div id="mobileMenu" className='hidden fixed top-16 right-0 bottom-0 left-0
+            p-5 md:hidden z-40 bg-black/70 backdrop-blur-sm 
+            bg-opacity-30
+            '>
+                <nav className='flex flex-col gap-6 items-center'>
+                    <a className='text-base tracking-wider
+                    transition-colors hover:text-gray-300
+                    z-50' href="#">COMPANY</a>
+
+                    <a className='text-base tracking-wider
+                    transition-colors hover:text-gray-300
+                    z-50' href="#">FEATURES</a>
+
+                    <a className='text-base tracking-wider
+                    transition-colors hover:text-gray-300
+                    z-50' href="#">RESOURCES</a>
+
+                    <a className='text-base tracking-wider
+                    transition-colors hover:text-gray-300
+                    z-50' href="#">DOCS</a>
+
+                </nav>
+            </div>
+
+        </header>
+    )
+}
+
+export default Header
